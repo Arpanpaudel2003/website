7bd96d946d030e0da4b39c6a642e84e5cde553aa
arpanpaudel2003 <arpanpaudel999@gmail.com>
2025-01-30 22:30:52 +0545

    third

--- api/views.py
+++ api/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm, RegisterForm
 from .models import Post
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 
+@login_required(login_url='login')
 def blog(request):
-    # Get the search query from GET parameters
     search_query = request.GET.get('search', '')
     
-    # Filter posts based on the search query if provided
     if search_query:
-        posts = Post.objects.filter(para__icontains=search_query)  # Adjust to your field
+        posts = Post.objects.filter(para__icontains=search_query) 
     else:
         posts = Post.objects.all()
     
@@ -27,6 +27,7 @@ def blog(request):
     context = {'posts': posts, 'form': form, 'search_query': search_query}
     return render(request, 'blog.html', context)
 
+@login_required(login_url='login')
 def deletePost(request, pk):
     item = Post.objects.get(id=pk)
     
@@ -37,7 +38,7 @@ def deletePost(request, pk):
     context = {'item': item}
     return render(request, 'delete.html', context)
 
-
+@login_required(login_url='login')
 def updatePost(request, pk):
     post = Post.objects.get(id=pk)
     form = PostForm(instance=post)
@@ -51,21 +52,22 @@ def updatePost(request, pk):
     context = {'form': form}
     return render(request, "update_post.html", context)
 
+@login_required(login_url='login')
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     return render(request, 'post_detail.html', {'post': post})
 
 def register_view(request):
-    if request.method == "POST":
-        form = RegisterForm(request.POST)
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request,user)
-            messages.success(request, "Register Successfully")
-            return redirect('home')
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}. You can now log in.')
+            return redirect('login')
     else:
-        form = RegisterForm()
-    return render(request,'register.html', {'form': form})
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
 
 def login_view(request):
     if request.method == "POST":
@@ -74,14 +76,14 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, "Login Successfully")
-            return redirect('home')
+            #messages.success(request, "Login Successfully")
+            return redirect('blog')
         else:
             messages.error(request, "Invalid username or password")
     return render(request, 'login.html')
 
 def logout_view(request):
     logout(request)
-    messages.info(request, "Logout Successfully")
-    return redirect('home')
+    #messages.info(request, "Logout Successfully")
+    return redirect('login')
         
